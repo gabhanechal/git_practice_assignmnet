486793979fca6a9936fc5da139dc9e326ddce7ad
gabhanechal <119440553+gabhanechal@users.noreply.github.com>
2022-12-06 19:13:24 -1200

    fixed for prime

--- prime.js
+++ prime.js
@@ -1,10 +1,16 @@
 
 function checkPrime(number){
+let ans =0;
 for (let i=1;i<=number;i++){
 if(number%i==2){
-return true;
+ans++
+}
+
+}if(ans==2){
+console.log("Prime");
+}
+else{
+console.log("Not")
 }
 }
-return false;
-}
-checkPrime(1);
+checkPrime(13);
